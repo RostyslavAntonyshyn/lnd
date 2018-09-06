78e58c3038af42091fd94af1550da6f7d3ff1e43
Offer Markovich <offerm@PayKey.com>
2018-09-06 11:46:13 +0300

    htlcswitch/link: add hash resolver
       This commit introduces an optional feature to resolve a hash via an
       external service. If we receive a payment for which we are the final
       destination, and yet LookupInvoice returns an error indicating an
       unknown hash, we can attempt to retrieve the preimage for this hash
       from the external hash resolver service before failing the htlc. This allows for payment hashes and preimages to be created, stored, and retrieved independently from lnd.

--- htlcswitch/link.go
+++ htlcswitch/link.go
@@ -319,6 +319,10 @@ type channelLink struct {
 	// sub-systems with the latest set of active HTLC's on our channel.
 	htlcUpdates chan []channeldb.HTLC
 
+	// resolver is a channel that we'll use to receive preimage
+	// resolution from the async hash resolver.
+	resolver chan resolutionData
+
 	// logCommitTimer is a timer which is sent upon if we go an interval
 	// without receiving/sending a commitment update. It's role is to
 	// ensure both chains converge to identical state in a timely manner.
@@ -351,6 +355,7 @@ func NewChannelLink(cfg ChannelLinkConfig,
 		overflowQueue:  newPacketQueue(lnwallet.MaxHTLCNumber / 2),
 		htlcUpdates:    make(chan []channeldb.HTLC),
 		quit:           make(chan struct{}),
+		resolver:       make(chan resolutionData, 1),
 	}
 }
 
@@ -955,6 +960,39 @@ out:
 					"unable to update commitment: %v", err)
 				break out
 			}
+
+		case resolution := <-l.resolver:
+			log.Debugf("Starting to handle resolution")
+
+			if resolution.failed {
+				log.Errorf("resolution: Received failure from hash resolver")
+				failure := lnwire.FailUnknownPaymentHash{}
+				l.sendHTLCError(
+					resolution.pd.HtlcIndex, failure, resolution.obfuscator, resolution.pd.SourceRef,
+				)
+			} else {
+				// if we are here, we have the preImage and we can settle this HTLC
+				err := l.channel.SettleHTLC(
+					resolution.preimageArray, resolution.pd.HtlcIndex, resolution.pd.SourceRef, nil, nil,
+				)
+				// if there is an error, fail the link
+				if err != nil {
+					l.fail(LinkFailureError{code: ErrInternalError},
+						"resolution: unable to settle htlc: %v", err)
+					continue
+				}
+
+				l.cfg.Peer.SendMessage(false, &lnwire.UpdateFulfillHTLC{
+					ChanID:          resolution.l.ChanID(),
+					ID:              resolution.pd.HtlcIndex,
+					PaymentPreimage: resolution.preimageArray,
+				})
+			}
+
+			log.Debugf("resolution: resuming BatchTicker")
+
+			l.batchCounter++
+			l.cfg.BatchTicker.Resume()
 
 		case <-l.cfg.BatchTicker.Ticks():
 			// If the current batch is empty, then we have no work
@@ -2310,6 +2348,12 @@ func (l *channelLink) processRemoteAdds(fwdPkg *channeldb.FwdPkg,
 				invoiceHash,
 			)
 			if err != nil {
+				if isResolverActive() {
+					// fire async resolver and return. HTLC will be handled by the resolver
+					asyncResolve(pd, l, obfuscator, heightNow)
+					continue
+				}
+
 				log.Errorf("unable to query invoice registry: "+
 					" %v", err)
 				failure := lnwire.FailUnknownPaymentHash{}
